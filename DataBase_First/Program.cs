4eaf85269fa502b975451d428dac2833b5b6bbb3
arevik.naghdalyan <arevik.naghdalyan@B7F1HR-02>
2019-06-09 23:22:15 +0400

    Add project files.

---
+++ DataBase_First/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DataBase_First
+{
+    class Program
+    {
+        static void Main()
+        {
+            var context = new PersonEntities2();
+            foreach (var person in context.People)
+            {
+                Console.WriteLine("{0}: {1,6} - {2} years", person.Id, person.Name, person.Age);
+            }
+
+
+        }
+    }
+}
